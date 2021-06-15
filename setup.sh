5678a374926bf780315b731a23fe55acd5cc8c13
Matias Rozanec <rozanecm@gmail.com>
2021-06-15 19:05:07 -0300

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ sudo apt install python3-virtualenv -y
 virtualenv -p python3 venv
 source venv/bin/activate && pip install -r requirements.txt
 sudo apt-get install qt5-default -y
+sudo apt-get install qttools5-dev-tools -y
