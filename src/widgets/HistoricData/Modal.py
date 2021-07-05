56994d3d6dc6c1e5deee6d7d11764dd9ba1db19d
Matias Rozanec <rozanecm@gmail.com>
2021-07-05 16:30:46 -0300

    feat: persist state of elected choices in modal to show/hide lines in chart

--- src/widgets/HistoricData/Modal.py
+++ src/widgets/HistoricData/Modal.py
@@ -1,9 +1,8 @@
 from typing import Dict
 
-from PyQt5 import QtGui
 from PyQt5.QtChart import QSplineSeries
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QCheckBox
+from PyQt5.QtWidgets import QDialog, QVBoxLayout
 from typing_extensions import TypedDict
 
 from src.widgets.GroupBox.GroupBox import GroupBox
@@ -23,7 +22,7 @@ class Modal(QDialog):
         self.chart_settings_groupbox = GroupBox("Chart Settings")
         self.layout.addWidget(self.chart_settings_groupbox)
 
-        self.markers = markers
+        self.line_series = markers
         self.line_buttons = {}
 
         self.setMinimumWidth(250)
@@ -32,15 +31,12 @@ class Modal(QDialog):
 
     def set_gui(self):
         self.chart_settings_groupbox.setLayout(QVBoxLayout())
-        for marker in self.markers:
-            # current_marker = QCheckBox()
-            current_marker = CheckBox(marker, parent=self)
-            current_marker.setText(str(marker))
-            self.line_buttons[marker] = current_marker
+        for marker_id, value in self.line_series.items():
+            current_marker = CheckBox(marker_id, parent=self)
+            current_marker.setText(str(marker_id))
+            self.line_buttons[marker_id] = current_marker
             self.chart_settings_groupbox.layout().addWidget(current_marker)
-            # current_marker.stateChanged.connect(lambda: self.toggle(marker))
-            # current_marker.clicked.connect(lambda: self.toggle(marker))
-            # self.button.clicked.connect(lambda: self.process_csv_click())
+            current_marker.setChecked(value['is_visible'])
 
     def toggle(self, marker: int, new_value: bool):
         self.parent().enable_line_toggle(marker, new_value)
