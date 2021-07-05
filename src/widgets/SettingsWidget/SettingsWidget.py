5407f9d7adde1631434270d0a6077ce6b3b48529
Matias Rozanec <rozanecm@gmail.com>
2021-07-05 18:28:58 -0300

    refact: clean settings widget

--- src/widgets/SettingsWidget/SettingsWidget.py
+++ src/widgets/SettingsWidget/SettingsWidget.py
@@ -6,25 +6,28 @@ from src.widgets.GroupBox.GroupBox import GroupBox
 class SettingsWidget(GroupBox):
     def __init__(self, parent=None):
         super().__init__("Configuración", parent=parent)
+        self.setMinimumWidth(150)
         self.layout = QVBoxLayout()
-
         self.setLayout(self.layout)
 
-        self.delta_t_layout = QHBoxLayout()
         self.delta_t_input = QSpinBox()
-        delta_t_label = QLabel("Delta t")
-        self.delta_t_layout.addWidget(delta_t_label)
-        self.delta_t_layout.addWidget(self.delta_t_input)
-
-        self.ppm_layout = QHBoxLayout()
         self.ppm_input = QSpinBox()
+
+        self.layout.addStretch()
+        self.setup_delta_t()
+        self.setup_ppm()
+        self.layout.addStretch()
+
+    def setup_ppm(self):
+        ppm_layout = QHBoxLayout()
         ppm_label = QLabel("ppm")
-        self.ppm_layout.addWidget(ppm_label)
-        self.ppm_layout.addWidget(self.ppm_input)
+        ppm_layout.addWidget(ppm_label)
+        ppm_layout.addWidget(self.ppm_input)
+        self.layout.addLayout(ppm_layout)
 
-        self.layout.addStretch()
-        self.layout.addLayout(self.delta_t_layout)
-        self.layout.addLayout(self.ppm_layout)
-        self.layout.addStretch()
-
-        self.setMinimumWidth(150)
+    def setup_delta_t(self):
+        delta_t_layout = QHBoxLayout()
+        delta_t_label = QLabel("Delta t")
+        delta_t_layout.addWidget(delta_t_label)
+        delta_t_layout.addWidget(self.delta_t_input)
+        self.layout.addLayout(delta_t_layout)
