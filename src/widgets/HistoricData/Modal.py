e09c3cf96b88317a3492d1adb77dcbf92496579f
Matias Rozanec <rozanecm@gmail.com>
2021-07-05 17:10:02 -0300

    feat: add delt t and ppm input in chart modal

--- src/widgets/HistoricData/Modal.py
+++ src/widgets/HistoricData/Modal.py
@@ -1,7 +1,7 @@
 from typing import Dict
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QVBoxLayout
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QSpinBox, QLabel
 
 from src.widgets.GroupBox.GroupBox import GroupBox
 from src.widgets.HistoricData.CheckBox import CheckBox
@@ -14,16 +14,32 @@ class Modal(QDialog):
         self.line_series: Dict[int, line] = {}
 
         self.setWindowModality(Qt.ApplicationModal)
-        self.layout = QVBoxLayout()
+        self.layout = QHBoxLayout()
         self.setLayout(self.layout)
 
         self.chart_settings_groupbox = GroupBox("Chart Settings")
         self.layout.addWidget(self.chart_settings_groupbox)
+        self.right_side_layout = QVBoxLayout()
+        delta_t_layout = QHBoxLayout()
+        delta_t_label = QLabel("Delta t")
+        delta_t_input = QSpinBox()
+        delta_t_layout.addWidget(delta_t_label)
+        delta_t_layout.addWidget(delta_t_input)
+
+        ppm_layout = QHBoxLayout()
+        ppm_label = QLabel("ppm")
+        ppm_input = QSpinBox()
+        ppm_layout.addWidget(ppm_label)
+        ppm_layout.addWidget(ppm_input)
+
+        self.right_side_layout.addLayout(delta_t_layout)
+        self.right_side_layout.addLayout(ppm_layout)
+        self.layout.addLayout(self.right_side_layout)
 
         self.line_series = line_series
         self.line_buttons = {}
 
-        self.setMinimumWidth(250)
+        self.setMinimumWidth(300)
 
         self.set_gui()
 
