9c1fafb0a3545ffc0c468d33980ae28de74586ca
Matias Rozanec <rozanecm@gmail.com>
2021-07-09 11:13:19 -0300

    (feat): connect modifymarkerspositionwidget widgets to slots

--- src/widgets/MarkersManagement/ModifyMarkersPositionWidget.py
+++ src/widgets/MarkersManagement/ModifyMarkersPositionWidget.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QComboBox, QLabel, QSpinBox
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QComboBox, QLabel, QSpinBox, QStyle
 
 
 class ModifyMarkersPositionWidget(QWidget):
@@ -14,6 +14,7 @@ class ModifyMarkersPositionWidget(QWidget):
         self.layout.addWidget(marker_selector_label)
 
         marker_selector_combo_box = QComboBox()
+        marker_selector_combo_box.textActivated.connect(lambda x: self.update_combo_box_value(x))
         marker_selector_combo_box.addItem("1")
         marker_selector_combo_box.addItem("2")
         marker_selector_combo_box.addItem("3")
@@ -24,10 +25,21 @@ class ModifyMarkersPositionWidget(QWidget):
 
         pos_x_label = QLabel("pos x")
         pos_x_spinbox = QSpinBox()
+        pos_x_spinbox.valueChanged.connect(lambda value: self.pos_x_spin_changed(value))
         pos_y_label = QLabel("pos y")
         pos_y_spinbox = QSpinBox()
+        pos_y_spinbox.valueChanged.connect(lambda value: self.pos_y_spin_changed(value))
 
         self.layout.addWidget(pos_x_label)
         self.layout.addWidget(pos_x_spinbox)
         self.layout.addWidget(pos_y_label)
         self.layout.addWidget(pos_y_spinbox)
+
+    def update_combo_box_value(self, text):
+        print(text)
+
+    def pos_x_spin_changed(self, new_value):
+        print(new_value)
+
+    def pos_y_spin_changed(self, new_value):
+        print(new_value)
