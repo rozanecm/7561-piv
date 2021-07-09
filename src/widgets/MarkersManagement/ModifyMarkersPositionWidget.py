733f16ac86733a873fe17fdb8eb47d456bf1992e
Matias Rozanec <rozanecm@gmail.com>
2021-07-09 11:40:03 -0300

    (feat): disable update pos spinboxes at startup

--- src/widgets/MarkersManagement/ModifyMarkersPositionWidget.py
+++ src/widgets/MarkersManagement/ModifyMarkersPositionWidget.py
@@ -19,19 +19,17 @@ class ModifyMarkersPositionWidget(GroupBox):
 
         marker_selector_combo_box = QComboBox()
         marker_selector_combo_box.textActivated.connect(lambda x: self.update_combo_box_value(x))
-        marker_selector_combo_box.addItem("1")
-        marker_selector_combo_box.addItem("2")
-        marker_selector_combo_box.addItem("3")
-        marker_selector_combo_box.addItem("4")
         self.layout.addWidget(marker_selector_combo_box)
 
         self.layout.addStretch()
 
         pos_x_label = QLabel("pos x")
         pos_x_spinbox = QSpinBox()
+        pos_x_spinbox.setEnabled(False)
         pos_x_spinbox.valueChanged.connect(lambda value: self.pos_x_spin_changed(value))
         pos_y_label = QLabel("pos y")
         pos_y_spinbox = QSpinBox()
+        pos_y_spinbox.setEnabled(False)
         pos_y_spinbox.valueChanged.connect(lambda value: self.pos_y_spin_changed(value))
 
         self.layout.addWidget(pos_x_label)
