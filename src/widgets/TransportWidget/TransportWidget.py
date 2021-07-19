81aea9c326a168a7d6f74230ca4f1d29ccc64267
Matias Rozanec <rozanecm@gmail.com>
2021-07-19 17:01:03 -0300

    refact(transport): abstract button creation to generic func

--- src/widgets/TransportWidget/TransportWidget.py
+++ src/widgets/TransportWidget/TransportWidget.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from PyQt5.QtWidgets import QHBoxLayout, QPushButton, QVBoxLayout
 
 from src.widgets.GroupBox.GroupBox import GroupBox
@@ -11,27 +13,17 @@ class TransportWidget(GroupBox):
         self.set_gui()
 
     def set_gui(self):
-        play_button = self.make_play_button()
-        stop_button = self.make_stop_button()
-        get_preview_button = self.make_get_preview_button()
+        play_button = self.make_button("Iniciar", self.process_play_button_click)
+        stop_button = self.make_button("Detener", self.process_stop_button_click)
+        get_preview_button = self.make_button("Obtener previsualizacion", self.process_get_preview_button_click)
         self.layout.addWidget(play_button)
         self.layout.addWidget(stop_button)
         self.layout.addWidget(get_preview_button)
 
-    def make_get_preview_button(self):
-        get_preview_button = QPushButton("Obtener previsualizacion")
-        get_preview_button.clicked.connect(lambda: self.process_get_preview_button_click())
-        return get_preview_button
-
-    def make_stop_button(self):
-        stop_button = QPushButton("Detener")
-        stop_button.clicked.connect(lambda: self.process_stop_button_click())
-        return stop_button
-
-    def make_play_button(self):
-        play_button = QPushButton("Iniciar")
-        play_button.clicked.connect(lambda: self.process_play_button_click())
-        return play_button
+    def make_button(self, label: str, func: Callable) -> QPushButton:
+        new_button = QPushButton(label)
+        new_button.clicked.connect(lambda: func())
+        return new_button
 
     def process_play_button_click(self):
         print("play button pressed")
