74fef90285c05e9c4a6b1a1e504f0c5dee56223e
Matias Rozanec <rozanecm@gmail.com>
2021-07-23 19:22:14 -0300

    feat(transport-buttons): disable start/stop if preview not gotten yet and if there are no markers set

--- src/widgets/TransportWidget/TransportWidget.py
+++ src/widgets/TransportWidget/TransportWidget.py
@@ -10,26 +10,28 @@ from src.widgets.GroupBox.GroupBox import GroupBox
 class TransportWidget(GroupBox):
     def __init__(self, main_window, parent=None):
         super().__init__("Control", parent=parent)
+        self.preview_button = self.make_button("Obtener previsualizacion",
+                                               os.path.join(os.path.dirname(__file__),
+                                                            "../../../res/get_preview_icon.png"),
+                                               self.process_get_preview_button_click)
+        self.start_button = self.make_button("Iniciar",
+                                             os.path.join(os.path.dirname(__file__), "../../../res/play_icon.png"),
+                                             self.process_play_button_click)
+        self.stop_button = self.make_button("Detener",
+                                            os.path.join(os.path.dirname(__file__), "../../../res/pause_icon.png"),
+                                            self.process_stop_button_click)
         self.main_window = main_window
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
         self.set_gui()
 
     def set_gui(self):
+        self.disable_start_stop_buttons()
         play_stop_layout = QHBoxLayout()
         self.layout.addLayout(play_stop_layout)
-        self.layout.addWidget(self.make_button("Obtener previsualizacion",
-                                               os.path.join(os.path.dirname(__file__),
-                                                            "../../../res/get_preview_icon.png"),
-                                               self.process_get_preview_button_click))
-        play_stop_layout.addWidget(self.make_button("Iniciar",
-                                                    os.path.join(os.path.dirname(__file__),
-                                                                 "../../../res/play_icon.png"),
-                                                    self.process_play_button_click))
-        play_stop_layout.addWidget(self.make_button("Detener",
-                                                    os.path.join(os.path.dirname(__file__),
-                                                                 "../../../res/pause_icon.png"),
-                                                    self.process_stop_button_click))
+        self.layout.addWidget(self.preview_button)
+        play_stop_layout.addWidget(self.start_button)
+        play_stop_layout.addWidget(self.stop_button)
 
     @staticmethod
     def make_button(label: str, path: str, func: Callable) -> QPushButton:
@@ -48,3 +50,11 @@ class TransportWidget(GroupBox):
     def process_get_preview_button_click(self):
         self.main_window.get_img_sample = True
         self.main_window.image_widget.image.can_manipulate_markers = True
+
+    def enable_start_stop_buttons(self):
+        self.start_button.setDisabled(False)
+        self.stop_button.setDisabled(False)
+
+    def disable_start_stop_buttons(self):
+        self.start_button.setDisabled(True)
+        self.stop_button.setDisabled(True)
