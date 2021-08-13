f7a0f5bde42dcb367cf7bd25499469c2cc84bc5b
Matias Rozanec <rozanecm@gmail.com>
2021-08-13 01:08:32 -0300

    min size: 8->16

--- src/widgets/ImageWidget/selectionSize.py
+++ src/widgets/ImageWidget/selectionSize.py
@@ -21,10 +21,10 @@ class SelectionSizeWidget(GroupBox):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
-        self.radio_button_other = QRadioButton("otro")
-        self.radio_button_32x32 = QRadioButton("32x32")
         self.radio_button_16x16 = QRadioButton("16x16")
-        self.radio_button_8x8 = QRadioButton("8x8")
+        self.radio_button_32x32 = QRadioButton("32x32")
+        self.radio_button_64x64 = QRadioButton("64x64")
+        self.radio_button_other = QRadioButton("otro")
         self.other_value_spinner = QSpinBox()
 
         self.button_group = QButtonGroup()
@@ -38,30 +38,30 @@ class SelectionSizeWidget(GroupBox):
         self.setup_button_group()
 
     def setup_button_group(self):
-        self.button_group.addButton(self.radio_button_8x8)
         self.button_group.addButton(self.radio_button_16x16)
         self.button_group.addButton(self.radio_button_32x32)
+        self.button_group.addButton(self.radio_button_64x64)
         self.button_group.addButton(self.radio_button_other)
         self.button_group.buttonPressed.connect(lambda button: self.button_pressed(button))
 
     def initial_radio_button_check(self):
-        self.radio_button_8x8.setChecked(True)
+        self.radio_button_16x16.setChecked(True)
         self.settings_bearer.update_settings(Constants.SETTINGS_SELECTION_SIZE,
-                                             selection_size_as_int(self.radio_button_8x8.text().split("x")[0]))
+                                             selection_size_as_int(self.radio_button_16x16.text().split("x")[0]))
         self.parent.roi_selector_widget.set_min_value()
 
     def set_other_size_radio_button(self):
         other_option_layout = QHBoxLayout()
         other_option_layout.addWidget(self.radio_button_other)
         other_option_layout.addWidget(self.other_value_spinner)
-        self.other_value_spinner.setMinimum(2)
+        self.other_value_spinner.setMinimum(16)
         self.other_value_spinner.valueChanged.connect(lambda val: self.other_value_spinner_changed(val))
         self.layout.addLayout(other_option_layout)
 
     def set_numeric_radio_buttons(self):
-        self.layout.addWidget(self.radio_button_8x8)
         self.layout.addWidget(self.radio_button_16x16)
         self.layout.addWidget(self.radio_button_32x32)
+        self.layout.addWidget(self.radio_button_64x64)
 
     def button_pressed(self, button: QRadioButton):
         if button.text() == "otro":
